e3ff824bace83a8b6665260a0433152e03e3161a
haoyifen <526667233@qq.com>
2016-01-08 23:15:25 +0800

    firstCommit

---
+++ JavaSELearing/src/main/java/org/haoyifen/JavaSELearing/Reflection/ReflectionTimeAnalysis.java
@@ -0,0 +1,48 @@
+package org.haoyifen.JavaSELearing.Reflection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ReflectionTimeAnalysis {
+
+	private static int createTimes = 1000000;
+	private static Random random = new Random();
+	public float f;
+
+	public ReflectionTimeAnalysis() {
+		super();
+	}
+
+	public static void main(String[] args) throws Exception {
+		doRegular();
+		doReflection();
+	}
+
+	private static void doReflection() throws Exception {
+		long startTime = System.currentTimeMillis();
+//		把生成的对象加入list,防止生成过程被优化
+		List<ReflectionTimeAnalysis> list = new ArrayList<>();
+		for (int i = 0; i < createTimes; i++) {
+			ReflectionTimeAnalysis object = (ReflectionTimeAnalysis) Class
+					.forName("org.haoyifen.JavaSELearing.Reflection.ReflectionTimeAnalysis").newInstance();
+			object.f = random.nextFloat();
+			list.add(object);
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("reflection cost " + (endTime - startTime) + "ms");//reflection cost 2864ms
+	}
+
+	private static void doRegular() {
+		long startTime = System.currentTimeMillis();
+		List<ReflectionTimeAnalysis> list = new ArrayList<>();
+		for (int i = 0; i < createTimes; i++) {
+			ReflectionTimeAnalysis object = new ReflectionTimeAnalysis();
+			object.f = random.nextFloat();
+			list.add(object);
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("regular cost " + (endTime - startTime) + "ms");//regular cost 68ms
+	}
+
+}
